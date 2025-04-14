ef34021985ea2a13da5a8cb60610d5ce8dcfd981
Randy Gonzalez <gonzalezmanaurer@gmail.com>
2025-04-13 20:02:08 -0400

    se agrego el test en los archivos server-app y app.ts

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,3 +1,31 @@
-describe('Test on App', () => { 
-    test('should first', () => {  })
- })
+import { ServerApp } from "./presentation/server-app";
+
+describe("Test on App.ts", () => {
+
+  test("should  call Server.run with values", async () => {
+    const serverRunMock = jest.fn();
+    ServerApp.run = serverRunMock;
+
+    process.argv = [
+      "node",
+      "app.ts",
+      "-b",
+      "10",
+      "-l",
+      "20",
+      "-s",
+      "-n",
+      "test-file",
+      "-d",
+      "test-destination",
+    ];
+    await import("./app");
+    expect(serverRunMock).toHaveBeenCalledWith({
+      base: 10,
+      limit: 20,
+      showTable: true,
+      fileName: "test-file",
+      fileDestination: "test-destination",
+    });
+  });
+});
--- src/presentation/server-app.test.ts
+++ src/presentation/server-app.test.ts
@@ -1,1 +1,59 @@
+import { CreateTable } from "../domain/use-cases/create-table.use-case";
+import { SaveFile } from "../domain/use-cases/save-file.use.case";
+import { ServerApp } from "./server-app";
+describe("test on server app", () => {
+  beforeEach(()=>{
+    jest.clearAllMocks();
+  })
+  const options = {
+    base: 2,
+    limit: 10,
+    showTable: false,
+    fileDestination: "test-destination",
+    fileName: "test-fileName",
+  };
+  const { base, limit, fileDestination, fileName } = options;
+  test("should create ServerApp instance", () => {
+    const serverApp = new ServerApp();
 
+    expect(serverApp).toBeInstanceOf(ServerApp);
+    expect(typeof ServerApp.run).toBe("function");
+  });
+  test("should run ServerApp with options", () => {
+    const logSpy = jest.spyOn(console, "log");
+    const createTableSpy = jest.spyOn(CreateTable.prototype, "execute");
+    const saveFileSpy = jest.spyOn(SaveFile.prototype, "execute");
+
+    ServerApp.run(options);
+    expect(logSpy).toHaveBeenCalledTimes(2);
+    expect(logSpy).toHaveBeenCalledWith("Server running....");
+    expect(logSpy).toHaveBeenLastCalledWith("File Created!");
+    expect(createTableSpy).toHaveBeenCalledWith({ base, limit });
+    expect(saveFileSpy).toHaveBeenCalledWith(
+      expect.objectContaining({
+        fileDestination,
+        fileName,
+      })
+    );
+  });
+
+  test("should run with custom values mocked", () => {
+    const createMock = jest.fn().mockReturnValue("1 x 1 = 2");
+    const saveFileMock = jest.fn().mockReturnValue(true);
+    const logMock = jest.fn();
+    const logErrorMock = jest.fn();
+    global.console.error = logErrorMock;
+    global.console.log = logMock;
+    CreateTable.prototype.execute = createMock;
+    SaveFile.prototype.execute = saveFileMock;
+    ServerApp.run(options);
+    expect(logMock).toHaveBeenCalledWith("Server running....");
+    expect(createMock).toHaveBeenCalledWith({ base, limit });
+    expect(saveFileMock).toHaveBeenCalledWith({
+      fileContent: expect.any(String),
+      fileDestination,
+      fileName,
+    });
+    expect(logMock).toHaveBeenCalledWith("File Created!");
+  });
+});
